108d36de95196014b39e4515052655f83973ae2c
Federico Sgambelluri <65596046+federicosgambelluri@users.noreply.github.com>
2026-01-05 14:18:34 +0100

    Fix: Update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for simplified GH Pages deployment
+  base: '/Bend-for-Abs/', // GitHub Pages repository name
 })
